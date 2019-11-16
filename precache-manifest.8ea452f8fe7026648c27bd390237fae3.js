6dd70f602c50f0707f7246c70b637772282806ee
brandnn <brandnn@uw.edu>
2019-11-16 10:53:23 -0800

    Updates

---
+++ precache-manifest.8ea452f8fe7026648c27bd390237fae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ed03e83e90204060b64c8e6b22025d",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "637964e490739c655d0d",
+    "url": "/Clicky-Game/static/css/main.5d8a100a.chunk.css"
+  },
+  {
+    "revision": "7953f95efca1b0977526",
+    "url": "/Clicky-Game/static/js/2.629f8e5c.chunk.js"
+  },
+  {
+    "revision": "637964e490739c655d0d",
+    "url": "/Clicky-Game/static/js/main.9b5fc901.chunk.js"
+  },
+  {
+    "revision": "00175ce074b0e8284c2e",
+    "url": "/Clicky-Game/static/js/runtime-main.5c08e3d6.js"
+  }
+]);
